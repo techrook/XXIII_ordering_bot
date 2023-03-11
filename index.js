2d607ae23bd732795bde65eb2bc1be89db8f0a0d
techrook <mondayitohowo3@gmail.com>
2023-03-11 01:34:05 +0100

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const cors = require("cors");
+
 
 const server = http.createServer(app);
 
 const { Server } = require("socket.io");
 const io = new Server(server);
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
