88c9e82df8bda326d3a4cd7bb14a277295445924
techrook <mondayitohowo3@gmail.com>
2023-03-11 01:08:05 +0100

    order history functionalities

--- index.js
+++ index.js
@@ -19,12 +19,35 @@ app.get("/", (req, res) => {
       console.log("client disconnected", socket.id);
     });
 
-    socket.on("order_created", (event) => {
-      console.log('order created')
+    socket.on("order_created", (data, callback) => {
+      console.log('order created', data)
+
+      callback({
+        status: "ok",
+        message: `You order as been created and that will cost you $${data.order_cost}. you can click 99 to check out this order`
+      });
+    });
+
+    socket.on("order checked out", (checked_out_order, callback) => {
+      console.log('order checked out', checked_out_order)
+
+      callback({
+        status: "ok",
+        message: `your ${checked_out_order.order}` + ` has been checked out and user account debited`
+      });
+    });
+
+    socket.on("order history", (all_orders, callback) => {
+      console.log('order history', all_orders)
+
+      callback({
+        status: "ok",
+        message: `you ordered ${all_orders}`
+      });
     });
 
 });
 
 server.listen(2323, () => {
     console.log("listening on *:2323");
-  });
+  })
