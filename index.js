e4b7f1c865f006d8210c52eba41512f4973873ef
techrook <mondayitohowo3@gmail.com>
2023-03-13 00:52:37 +0100

    current order functionalities

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 
-
 const server = http.createServer(app);
 
 const { Server } = require("socket.io");
@@ -12,46 +11,57 @@ const io = new Server(server);
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-  });
-
-  io.on("connection", (socket) => {
-    console.log("client connected", socket.id);
-    
-    
-    socket.on("disconnect", () => {
-      console.log("client disconnected", socket.id);
-    });
-
-    socket.on("order_created", (data, callback) => {
-      console.log('order created', data)
-
-      callback({
-        status: "ok",
-        message: `You order as been created and that will cost you $${data.order_cost}. you can click 99 to check out this order`
-      });
-    });
-
-    socket.on("order checked out", (checked_out_order, callback) => {
-      console.log('order checked out', checked_out_order)
-
-      callback({
-        status: "ok",
-        message: `your ${checked_out_order.order}` + ` has been checked out and user account debited`
-      });
-    });
-
-    socket.on("order history", (all_orders, callback) => {
-      console.log('order history', all_orders)
-
-      callback({
-        status: "ok",
-        message: `you ordered ${all_orders}`
-      });
-    });
-
+  res.sendFile(__dirname + "/index.html");
 });
 
+io.on("connection", (socket) => {
+  console.log("client connected", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("client disconnected", socket.id);
+  });
+
+  socket.on("order_created", (data, callback) => {
+    console.log("order created", data);
+
+    callback({
+      status: "ok",
+      message: `You order as been created and that will cost you $${data.order_cost}. you can click 99 to check out this order`,
+    });
+  });
+
+  socket.on("order checked out", (checked_out_order, callback) => {
+    console.log("order checked out", checked_out_order);
+
+    callback({
+      status: "ok",
+      message:
+        `your ${checked_out_order.order}` +
+        ` has been checked out and user account debited`,
+    });
+  });
+
+  socket.on("order history", (all_orders, callback) => {
+    console.log("order history", all_orders);
+
+    callback({
+      status: "ok",
+      message: `you ordered ${all_orders.order}`,
+    });
+  });
+  
+  socket.on("current order notification", (pending_order, callback) => {
+    console.log("order history", pending_order);
+  
+    callback({
+      status: "ok",
+      message: `you ordered ${pending_order.order}`,
+    });
+  });
+});
+
+
+
 server.listen(5000, () => {
-    console.log("listening on *:5000");
-  })
+  console.log("listening on *:5000");
+});
