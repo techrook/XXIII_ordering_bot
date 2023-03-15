4914253516192f8112c097862883f3f1d71ffe3a
techrook <mondayitohowo3@gmail.com>
2023-03-15 16:06:49 +0100

    implemented chat like styles,functionality

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.use(cors());
-app.use(express.static(__dirname +'/public/'))
+app.use(express.static(__dirname + "/public/"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
